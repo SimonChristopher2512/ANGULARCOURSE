57b63fee5d6d293e311954aea957ffd6e61f2d0b
SimonChristopher2512 <91723734+SimonChristopher2512@users.noreply.github.com>
2024-01-05 16:33:34 +0530

    Add files via upload

---
+++ AngularBasics/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularBasics';
+  message='Angular is a JavaScript Framework which is allows to create single page application.'
+  collegeName='Knowledge Institute Of Technology';
+}
